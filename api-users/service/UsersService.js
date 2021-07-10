c0e37b939f3e419aa849e1eb916311bd29b5e8c6
disam8853 <wang.sam33@gmail.com>
2021-07-11 03:29:42 +0800

    add db port

--- api-users/service/UsersService.js
+++ api-users/service/UsersService.js
@@ -7,12 +7,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
 const DB_USER = process.env.DB_USER
 const DB_NAME = process.env.DB_NAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 
 const pool = mysql.createPool({
   host: DB_HOST,
+  port: DB_PORT,
   user: DB_USER,
   database: DB_NAME,
   password: DB_PASSWORD,
